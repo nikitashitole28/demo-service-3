989583af92fab18f8a6bec48afa8166f3fcf8242
nikitashitole28 <nikitashitole28@gmail.com>
2022-11-04 22:51:38 +0530

    updated

---
+++ demoService-3/src/main/java/demo/testexample/DemoService3Application.java
@@ -0,0 +1,13 @@
+package demo.testexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoService3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoService3Application.class, args);
+	}
+
+}
